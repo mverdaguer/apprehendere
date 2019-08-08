5cdf36470b79c25af20b76c265ff6e0c017b10b4
Marcv Verdaguer <marc.verdaguer.ginestera@gmail.com>
2019-08-08 19:58:12 +0200

    Adding redirection to home (now exercises) when the title on the top left is clicked.

--- src/components/app/navigation-bar/app-navigation-bar.js
+++ src/components/app/navigation-bar/app-navigation-bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, withRouter } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -16,6 +16,7 @@ const useStyles = makeStyles(theme => ({
   },
   toolbarTitle: {
     flexGrow: 1,
+    cursor: 'pointer',
   },
   link: {
     margin: theme.spacing(1, 1.5),
@@ -23,32 +24,35 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-export default function NavigationBar() {
-  const classes = useStyles();
+const NavigationBar = withRouter(
+  ({ history }) => {
+    const classes = useStyles();
 
-  return (
-    <React.Fragment>
-      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Apprehendere
-          </Typography>
-          <nav>
-            <Link variant="button" color="textPrimary" component={RouterLink} to="/exercises" className={classes.link}>
-              Exercises
-            </Link>
-            <Link variant="button" color="textPrimary" component={RouterLink} to="/dashboard" className={classes.link}>
-              Dashboard
-            </Link>
-            <Link variant="button" color="textPrimary" component={RouterLink} to="/about" className={classes.link}>
-              About
-            </Link>
-          </nav>
-          <Button href="#" color="primary" variant="outlined" className={classes.link}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
-}
+    return (
+      <React.Fragment>
+        <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle} onClick={() => history.push("/")}>
+              Apprehendere
+            </Typography>
+            <nav>
+              <Link variant="button" color="textPrimary" component={RouterLink} to="/exercises" className={classes.link}>
+                Exercises
+              </Link>
+              <Link variant="button" color="textPrimary" component={RouterLink} to="/dashboard" className={classes.link}>
+                Dashboard
+              </Link>
+              <Link variant="button" color="textPrimary" component={RouterLink} to="/about" className={classes.link}>
+                About
+              </Link>
+            </nav>
+            <Button href="#" color="primary" variant="outlined" className={classes.link}>
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
+});
+
+export default NavigationBar;
